2f4bb559da9c4abe41d408bd163637192c0a8fbe
Julian Tran <juliantran003@gmail.com>
2021-05-16 16:57:22 +0200

    trying limit

--- routes/characters.js
+++ routes/characters.js
@@ -5,9 +5,11 @@ const axios = require("axios");
 router.get("/characters", async (req, res) => {
   console.log("Using Route : /characters");
   try {
-    const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MY_MARVEL_API}`
-    );
+    const response = await axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MY_MARVEL_API}`
+      )
+      .limit(20);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(400).json({ error: error.message });
